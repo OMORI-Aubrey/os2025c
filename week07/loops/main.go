3dff9813b655eaa19e8be6fe844575f937d6ab0c
JaeYun <sim_518@naver.com>
2025-11-02 23:50:57 +0900

    loops/main.go v0.1 if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n') // 에러 무시
+	// i, _ := r.ReadString('\n') // 에러 무시
+	i, err := r.ReadString('\n')
+	// fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) // 애러 메시지 보고하고 프로그램 종료
+	}
+	
 	fmt.Println(i)
 }
