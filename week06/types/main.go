537500b17b64ac6041f18f68b40eae6f619d5e65
JaeYun <sim_518@naver.com>
2025-11-02 22:49:28 +0900

    v0.3 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Floor(2.21))
-	fmt.Println(math.Ceil(2.21))
-	fmt.Println(math.Round(2.21))
-
-	fmt.Println(strings.Title("go developer!"))
-
-	fmt.Println("Kim\nInha\t\"\\")
-	fmt.Println('2', '가')
+	fmt.Println(reflect.TypeOf(2.34))
+	fmt.Println(reflect.TypeOf("Kim Inha"))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf('A'))
+	fmt.Println(reflect.TypeOf(19))
 }
